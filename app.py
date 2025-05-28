57bbc464bafd3f04e45feb59d3e630169254d4e9
datthanhtran68 <118030101+datthanhtran68@users.noreply.github.com>
2025-05-28 20:26:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_login import LoginManager
 from pymongo import MongoClient
@@ -5,29 +6,28 @@ from routes.auth import auth_bp
 from routes.games import games_bp
 from routes.skins import skins_bp
 from models.user import load_user
-import os
 
 app = Flask(__name__)
-app.secret_key = 'super_secret_key_123'  # Thay bằng key an toàn trong production
+app.secret_key = 'super_secret_key_123'
 
-# Kết nối MongoDB
-client = MongoClient('mongodb://localhost:27017/')
+# Sửa ở đây
+mongo_uri = os.environ.get("MONGO_URI")
+client = MongoClient(mongo_uri)
+
 db = client['skin_manager']
 app.config['games_collection'] = db['games']
 app.config['skins_collection'] = db['skins']
 app.config['users_collection'] = db['users']
 
-# Cấu hình Flask-Login
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'auth.index'
 login_manager.user_loader(load_user)
 
-# Đăng ký blueprints
 app.register_blueprint(auth_bp)
 app.register_blueprint(games_bp, url_prefix='/api')
 app.register_blueprint(skins_bp, url_prefix='/api')
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render sẽ set biến PORT
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
